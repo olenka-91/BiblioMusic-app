6e4472b352d2e7bb2f5fdc732a42de018178c356
olenka-91 <olenka--91@mail.ru>
2024-11-20 16:10:10 +0300

    DB connection created

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 
+	"github.com/olenka-91/BIBLIOMUSIC-APP/internal/repository"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -14,4 +15,16 @@ func init() {
 
 func main() {
 
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Username: os.Getenv("DB_USERNAME"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   os.Getenv("BD_NAME"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+	})
+	if err != nil {
+		log.WithField("err:", err.Error()).Error("Couldn't create DB connection!")
+
+	}
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/olenka-91/BIBLIOMUSIC-APP
 
 go 1.22.2
 
-require github.com/sirupsen/logrus v1.9.3
+require (
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/sirupsen/logrus v1.9.3
+)
 
 require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
